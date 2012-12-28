a494c978c106ca782af5033094f8c4c8ff199aab
Jesus Macias Portela <jmacias@full-on-net.com>
2012-12-28 10:07:28 +0100

    Modulo GCM funcionando bien

--- Resources/gcm.js
+++ Resources/gcm.js
@@ -9,48 +9,35 @@ contentText = 'Received at ' + now.getHours() + ':' + now.getMinutes() + ':' + n
 // ****************************************************************************************************************
 // getting the payload
 
-Ti.API.info("GLEB - SERVICE GCM - ServiceIntent: "+ JSON.stringify(serviceIntent));
-
-Ti.API.info("GLEB - SERVICE GCM - PushId: "+ serviceIntent.getStringExtra('pushId'));
-Ti.API.info("GLEB - SERVICE GCM - Message: "+ serviceIntent.getStringExtra('message'));
-Ti.API.info("GLEB - SERVICE GCM - Payload: "+ serviceIntent.getStringExtra('payload'));
-
-
-
-if (serviceIntent.hasExtra('message')) {
-	tickerText = serviceIntent.getStringExtra('message');
+if (serviceIntent.hasExtra('alert')) {
+	tickerText = serviceIntent.getStringExtra('alert');
 }
 
 
 // ****************************************************************************************************************
 // intents & notification
 
-var act = Ti.Android.currentActivity;
-
 // we an intent and a pending intent in order to open the app when the users clicks on the notification
 var notificationIntent = Ti.Android.createIntent({
-    action : Ti.Android.ACTION_MAIN,
-    url : 'app.js',
-    flags : Ti.Android.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Ti.Android.FLAG_ACTIVITY_SINGLE_TOP
+	className: 'es.thinetic.ngleb.NewglebActivity',
+	packageName: 'es.thinetic.ngleb'
 });
-notificationIntent.addCategory(Titanium.Android.CATEGORY_LAUNCHER);
 
 // Create a PendingIntent to tie together the Activity and Intent
 // check http://docs.appcelerator.com/titanium/2.1/index.html#!/api/Titanium.Android.PendingIntent
 var intent = Ti.Android.createPendingIntent({
-    activity: act,
     intent: notificationIntent,
-    type: Ti.Android.PENDING_INTENT_FOR_ACTIVITY,
-    flags: Ti.Android.FLAG_ACTIVITY_NO_HISTORY
+    flags: Titanium.Android.FLAG_UPDATE_CURRENT
 });
 
 // check http://docs.appcelerator.com/titanium/2.1/index.html#!/api/Titanium.Android.Notification
 var notification = Ti.Android.createNotification({
 	contentIntent: intent,
 	contentTitle: tickerText,
-	contentText: contentText,
+	contentText: tickerText,
 	tickerText: tickerText,
 	icon: Ti.App.Android.R.drawable.appicon,
+	// icon: Ti.App.Android.R.drawable.notification,
 	defaults:Titanium.Android.NotificationManager.DEFAULT_ALL,
 	flags : Titanium.Android.ACTION_DEFAULT | Titanium.Android.FLAG_AUTO_CANCEL | Titanium.Android.FLAG_SHOW_LIGHTS
 });
